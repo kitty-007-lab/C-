b1d5297d8d45325e74b627d7dc82135357235af5
kitty-007-lab <58553216+kitty-007-lab@users.noreply.github.com>
2020-05-25 14:44:02 -0400

    Add files via upload
    A quick program to calculate dog years to human years

---
+++ Doggy years calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() {
+//This version of the program is for dogs older than 2 years old.
+  double dog_age;
+  double later_years;
+  int human_years;
+  cout<<"Enter your dog's age\n";
+  cin>>dog_age;
+
+  later_years = 9.9 ;
+  human_years = dog_age * later_years;
+  string name;
+
+
+
+cout<< "Enter your dog's name\n";
+cin>>name;
+
+
+
+
+cout<< "my name is " << name << " I am " << human_years << " years old in human years ";
+
+
+}
+
